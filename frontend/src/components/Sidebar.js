c8ddd97e83e9a0be2f8cbe1c09f976e06fabd91e
sumy68 <sue.meyyem@outlook.com>
2026-05-13 14:44:09 +0200

    fix: Freischaltung Badge nur ausstehende Accounts + Urlaub

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -18,7 +18,7 @@ export default function Sidebar() {
     if (user.role === 'admin') {
       axios.get(`${API}/api/auth/pending`).then(res => setPendingCount(res.data.length)).catch(() => {});
       axios.get(`${API}/api/abwesenheiten/pending-urlaub`).then(res => setPendingCount(c => c + res.data.length)).catch(() => {});
-      axios.get(`${API}/api/abwesenheiten/pending-krank`).then(res => setPendingCount(c => c + res.data.length)).catch(() => {});
+
       axios.get(`${API}/api/abwesenheiten`).then(res => {
         const heute = new Date().toISOString().split('T')[0];
         const krankHeute = res.data.filter(a => a.typ === 'krank' && a.created_at?.split('T')[0] === heute).length;
